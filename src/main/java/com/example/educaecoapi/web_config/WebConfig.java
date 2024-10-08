1455b12a399414f7bc804419310c47065ce315c5
xavier <samuelpedroso08@gmail.com>
2024-10-08 07:57:15 -0300

    Subindo WebConfig

---
+++ src/main/java/com/example/educaecoapi/web_config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.educaecoapi.web_config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Mapeia todas as rotas
+                .allowedOrigins("http://localhost:3000") // Domínio permitido
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowCredentials(true);
+    }
+}
+
